ac2dcb71ad9ca88ab5444d937807ea3203d9bf39
Bryan Vela <55074395+bdvela@users.noreply.github.com>
2022-11-13 15:55:52 -0500

    update button experiments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,4 @@
-const showAlert = () =>
+const showAlertPremium = () =>
     alert('Gracias por interesarte en obtener un mes de prueba. Nos contactaremos contigo próximamente.');
+const showAlertFree = () =>
+    alert('Gracias por interesarte en probar nuestra app. Nos contactaremos contigo próximamente.');
